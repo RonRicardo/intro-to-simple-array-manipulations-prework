6e9bdb47f0bc1824712941e2efc95b298d35581c
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-27 23:21:55 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args
+end
+
+def using_shift
+end
+
+def shift_with_args
+end
+
